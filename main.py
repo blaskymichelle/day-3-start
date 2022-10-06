ca63df46623b12d060e7c61f555efc21abd01aad
Michelle <62445133+blaskymichelle@users.noreply.github.com>
2022-10-06 01:26:47 +0000

    bmi

--- main.py
+++ main.py
@@ -5,4 +5,17 @@ if height > 120:
   print("You can ride the rollercoater!")
 else:
   print("Sorry, you have to grow taller before you can ride.")
-  
+
+
+bmi=18
+bmi = round(weight / height ** 2)
+if bmi < 18.5:
+    print(f"Your bmi is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f" Your bmi is {bmi}, you have a normal weight.")    
+elif bmi < 30:
+    print(f" Your bmi is {bmi}, you are overweight.")    
+elif bmi < 35:
+    print(f" Your bmi is {bmi}, you are obese.")  
+else:
+    print(f" Your bmi is {bmi}, you are clinically obese.")       
